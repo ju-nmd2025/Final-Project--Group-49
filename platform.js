b5dcb7c27c9e7240f168bbd4632ee457dca094bb
TMABEAN <63044799+TMABEAN@users.noreply.github.com>
2025-12-16 22:59:28 +0100

    Left-right moving platform

--- platform.js
+++ platform.js
@@ -7,6 +7,16 @@ class Platform {
     this.broken = false;
     this.direction = random() > 0.5 ? 1 : -1;
     this.speed = 2;
-    this.originalX = x; // Moving platform
-    this.moveRange = 100; // Moving areas
+    this.originalX = x; // Cho moving platform - Moving platform
+    this.moveRange = 100; // Phạm vi di chuyển - Moving areas
   }
+  
+  update() {
+    if (this.type === 'moving') {
+      // Di chuyển qua lại
+      this.position.x += this.speed * this.direction;
+
+      // Đảo chiều khi đến giới hạn
+      if (abs(this.position.x - this.originalX) > this.moveRange) {
+        this.direction *= -1;
+      }
